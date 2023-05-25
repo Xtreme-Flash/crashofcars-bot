62f0d0df9cd2d753b092e7f02f903a868122a2ef
Xtreme-Flash <doorway125@gmail.com>
2023-05-25 21:24:52 +0530

    delpoyed commands for gobal guilds

--- deploy-commands.js
+++ deploy-commands.js
@@ -34,13 +34,12 @@ const rest = new REST().setToken(token);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommand(clientId),
 			{ body: commands },
 		);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
 		console.error(error);
 	}
 })();
