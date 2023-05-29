1626f60ac14ef1becb02b08b22768f5d99a45d4c
Xtreme-Flash <doorway125@gmail.com>
2023-05-29 21:39:53 +0530

    updated ping

--- commands/fun/ping.js
+++ commands/fun/ping.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,5 +7,10 @@ module.exports = {
         .setDescription('Replies with pong!'),
     async execute(interaction) {
         await interaction.reply('Pong!');
+        await wait(2000);
+        await interaction.editReply('Never Gonna Give You Up');
+        await wait(4000);
+        await interaction.editReply('hehe u got ||rickrolled!||');
+        await interaction.followUp({ content: 'now cry about it!', ephemeral: true});
     },
 };
