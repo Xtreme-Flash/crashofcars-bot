5b8721d27d5d5de123167cea28b0b12dcc95a45d
Xtreme-Flash <doorway125@gmail.com>
2023-05-29 21:40:52 +0530

    error correction

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,22 +1,22 @@
 const { Events } = require('discord.js');
-const { execute } = require('./ready');
 
 module.exports = {
-    name: Events.InteractionCreate,
-    async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
 
-        const command = interaction.client.command.get(interaction.commandName);
+		const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error exucuting ${interaction.commandName}`);
-        }
-    },
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+		}
+	},
 };
