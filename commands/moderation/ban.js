c0bf68767bcdb9df4f1dd97c0c229d53f335000b
Xtreme-Flash <doorway125@gmail.com>
2023-06-06 21:06:58 +0530

    adden button to ban command

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -19,8 +19,24 @@ module.exports = {
         async execute(interaction) {
             const target = interaction.options.getUser('target');
             const reason = interaction.options.getString('reason') ?? 'No reason provided';
+
+			const confirm = new ButtonBuilder()
+				.setCustomId('confirm')
+				.setLabel('Confirm Ban')
+				.setStyle(ButtonStyle.Danger);
+
+			const cancel = new ButtonBuilder()
+				.setCustomId('cancel')
+				.setLabel('Cancel')
+				.setStyle(ButtonStyle.Secondary);	
+
+			const row = new ActionRowBuilder()
+				.addComponents(cancel, confirm);
     
-            await interaction.reply(`Banning ${target.username} for reason: ${reason}`);
+            await interaction.reply({
+				content: `Are you sure you want to ban ${target} for reason: ${reason}?`,
+				components: [row],
+			});
             await interaction.guild.members.ban(target);
         },
 };
