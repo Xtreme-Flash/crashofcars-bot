228397f8904b23c5268d29cda3ef1ca44b42325e
Xtreme-Flash <doorway125@gmail.com>
2023-06-08 19:26:46 +0530

    added row

--- commands/game/drop.js
+++ commands/game/drop.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const { SlashCommandBuilder,ActionRowBuilder, AttachmentBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { execute } = require('../moderation/ban');
 const fs = require('node:fs');
 const path = require('path');
@@ -18,12 +18,37 @@ module.exports = {
             const idx = (len) => Math.floor(Math.random() * len);
             const files = fs.readdirSync('C:/Users/ASUS/Desktop/crashofcars-bot/commands/game/cards/');
             const myImage = files[idx(files.length)];
-            const messageContent = `Oh, ${interaction.user.username}! Here's your card: ${myImage}.`;
+            const messageContent = `Oh, ${interaction.user.username}! you got the ${myImage}.`;
             
-            await interaction.editReply({
+            const acceptButton = new ButtonBuilder()
+              .setCustomId('accept')
+              .setLabel('Accept')
+              .setStyle(ButtonStyle.Success);
+
+            const skipButton = new ButtonBuilder()
+              .setCustomId('skip')
+              .setLabel('Skip')
+              .setStyle(ButtonStyle.Secondary);
+
+            const row = new ActionRowBuilder()
+              .setComponents(acceptButton,skipButton);  
+
+            const response = await interaction.editReply({
+              files: [`C:/Users/ASUS/Desktop/crashofcars-bot/commands/game/cards/${myImage}`],
+              components: [row],
               content: messageContent,
-              files: [`C:/Users/ASUS/Desktop/crashofcars-bot/commands/game/cards/${myImage}`]
             });
+
+            const collectorFilter = i => i.user.id === interaction.user.id;
+
+            const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+            if (confirmation.customId === 'accept') {
+              await confirmation.update({ content: `${interaction.user.username} took the ${myImage}.`, components: [] });
+            } else if (confirmation.customId === 'skip') {
+              await confirmation.update({ content: `skipped!` ,components: [] });
+            }
+
           } catch (error) {
             console.error('An error occurred:', error);
           }
